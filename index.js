ad6ebc416644b7f5d171915c690813642df48844
Asif3359 <asifahammed359@gmail.com>
2024-01-14 22:56:38 +0600

    connect successfully sql database

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
 const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-app.get('/',(req,res)=>{
+const port = process.env.PORT || 5000;
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "college",
+})
+
+db.connect();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/student', (req, res) => {
+    const sqlGet = 'select * from student';
+    db.query(sqlGet, (error, result) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        const data = result;
+        res.send(data);
+    });
+});
+
+
+
+app.get('/', (req, res) => {
     res.send("hello World");
 })
 
-app.listen(3001, ()=>{
-    console.log('college server running on port 3001');
+
+
+app.listen(port, () => {
+    console.log('college server running on port 5000');
 })
